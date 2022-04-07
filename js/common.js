be0cb7cedbf2d5dd01128c52fef4111c8a852f6e
Jasur <jasfayz@gmail.com>
2022-04-07 21:53:23 +0500

    add other section && animation

--- js/common.js
+++ js/common.js
@@ -1,5 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener('DOMContentLoaded', function () {
+	
 	// $('body').hide()
+	$('.header__menu-btn').click(function () {
+		console.log('click');
+		$('.header__nav').toggleClass('show')
+	})
 
+	setTimeout(function () {
+		$('.intro').addClass('black')
+		$('.header__logo-room').addClass('white')
+	}, 1000)
+	AOS.init();
 })
